e394f0bf60c5a3a6598182acaec3e13aa6d773d8
Charles Robin <charles.am.robin@gmail.com>
2015-07-08 22:25:28 +0200

    Allows comments in brainfuck code

--- brainfuck.py
+++ brainfuck.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
 import sys
 
-
-class BrainfuckSyntaxException(Exception):
-    pass
-
-# bf_chars = ['.', ',', '[', ']', '<', '>', '+', '-']
+# Char from brainfuck syntax
+BF_CHARS = ['.', ',', '[', ']', '<', '>', '+', '-']
 # Cells have a cize of 8-bits so 255
 CELL_SIZE =  255
 
@@ -15,19 +12,16 @@ def matching_brackets(code):
     i = 0
     for pos, cmd in enumerate(code):
         if cmd == "[":
-            stack.append(pos);
+            stack.append(pos)
         elif cmd == "]":
-            if len(stack) == 0:
-                raise BrainfuckSyntaxException("Missing [ for a ]")
             prevpos = stack.pop()
             matching[prevpos] = pos
             matching[pos] = prevpos
-    if len(stack) != 0:
-        raise BrainfuckSyntaxException("Missing ] for a [")
     return matching
 
 def interpret(code):
     code = list(code)
+	code = filter(lambda x : x in BF_CHARS, code)
     
     cells = [0]
     dataptr = 0
@@ -50,7 +44,7 @@ def interpret(code):
         if command == "-":
             cells[dataptr] = cells[dataptr] - 1 if cells[dataptr] > 0 else CELL_SIZE
         
-        if command == ".": sys.stdout.write(chr(cells[dataptr]))
+        if command == ".": print chr(cells[dataptr]),
         if command == ",": cells[dataptr] = ord(raw_input()[0])
         
         if command == "[" and cells[dataptr] == 0:
@@ -64,7 +58,5 @@ def interpret(code):
     
 # should print "Hello World!"
 interpret('++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.')
-# Print 2 + 2 = 5
-interpret('-[----->+<]>-.--[--->++<]>.+++++++++++.-----------.[-->+++<]>++.--[--->++<]>.-[->++<]>-.+[-->+<]>+.-----[->++<]>-.')
 # ToLower
 interpret('+[ >,>++++[<++++++++>-]<.<]')
